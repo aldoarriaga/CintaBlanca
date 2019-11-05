d84ae4a394eb2538e60a93d25bee96c8aab38289
aldoarriaga <aldo.arriaga@gmail.com>
2019-11-04 21:53:53 -0600

    Bootstrap excercises

---
+++ 000_JavaScript_LinkedIn/script.js
@@ -0,0 +1,34 @@
+let date = new Date();
+// console.log('Today is ' + date);
+
+// document.body.innerHTML = '<h1>Today is ' + date +  '</h1>';
+// document.body.innerHTML = '<h1>Today is ' + date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear() +  '</h1>';
+
+// var sum = a + b;
+// console.log(sum);
+
+// var a = 5;
+// var b = 5;
+
+// a == b ? console.log('Match') : console.log('No Match');;
+
+var pens;
+pens = ['red', 'blue', 'navy', 'pink', 'black', 'orange'];
+
+console.log(pens);
+// pens.push('purple', 'white');
+console.log('Array length: ', pens.length);
+// console.log(pens);
+// pens.splice(3,2);
+// console.log(pens);
+
+// var newPens = pens.slice();
+// newPens.push('purple');
+// console.log('New Pens : ', newPens);
+
+// var newPens = pens.indexOf('black', 5);
+
+// console.log(newPens);
+
+console.log(pens.join('/'));
+
